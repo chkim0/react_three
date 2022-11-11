4d6a09ed3e7e435d196ad1f0b62877d1c4ef46af
chkim0 <kpes2002@naver.com>
2022-11-11 12:13:53 +0900

    투명한 유리재질

--- src/App.js
+++ src/App.js
@@ -2,48 +2,57 @@ import './scss/style.scss';
 import { Canvas, useFrame, extend, useThree } from 'react-three-fiber';
 import { useRef } from 'react';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import * as THREE from 'three';
 extend({ OrbitControls });
 
-//화면축을 변경할 수 있는 컴포넌트
 const Orbit = () => {
 	const { camera, gl } = useThree();
 	return <orbitControls args={[camera, gl.domElement]} />
 }
-
-const Box = (props) => {
-	const ref = useRef(null);
-	useFrame(() => {
-		ref.current.rotation.x += 0.01;
-		ref.current.rotation.y += 0.01;
-	});
+const Bulb = (props) => {
 	return (
-		<mesh ref={ref} {...props} >
-			<boxBufferGeometry />
-			<meshPhysicalMaterial color='blue' />
+		<mesh {...props}>
+			<pointLight castShadow intensity={1} color='white' />
+			<sphereBufferGeometry args={[0.5, 20, 20]} />
+			<meshPhongMaterial emissive='yellow' />
 		</mesh>
 	)
 }
-
+const Box = (props) => {
+	const ref = useRef(null);
+	useFrame(() => {
+		ref.current.rotation.x += 0.02;
+		ref.current.rotation.y += 0.02;
+	});
+	return (
+		<mesh ref={ref} {...props} castShadow receiveShadow >
+			<boxBufferGeometry />
+			<meshPhysicalMaterial color='white' roughness={0} clearcoat={1} transparent transmission={0.5} reflectivity={1} side={THREE.DoubleSide} />
+		</mesh>
+	)
+}
 const Floor = (props) => {
 	return (
-		<mesh {...props}>
+		<mesh {...props} receiveShadow>
 			<boxBufferGeometry args={[20, 1, 10]} />
 			<meshPhysicalMaterial color='white' />
 		</mesh>
 	)
 }
-
 function App() {
 	return (
 		<figure>
 			<Canvas
+				//그림자 설정시 필요함
+				shadowMap
 				style={{ background: '#111' }}
-				camera={{ position: [3, 3, 3] }}>
+				camera={{ position: [3, 5, 3] }}>
 				<axesHelper args={[6]} />
 				<Orbit />
 
 				<ambientLight intensity={0.2} />
-				<Box position={[-1, 1, 2]} />
+				<Bulb position={[0, 3, 0]} />
+				<Box position={[-1, 2, 1]} />
 				<Floor position={[0, -0.5, 0]} />
 			</Canvas>
 		</figure>
