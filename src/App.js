1433b67b9f4f2e21bf31fb62bda523f6fbf36abf
chkim0 <kpes2002@naver.com>
2022-11-11 11:10:53 +0900

    ambientLight로 전체 밝기 조절 (그림자 생성X)

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 extend({ OrbitControls });
 
 //화면축을 변경할 수 있는 컴포넌트
-
 const Orbit = () => {
 	const { camera, gl } = useThree();
 	return <orbitControls args={[camera, gl.domElement]} />
@@ -13,17 +12,23 @@ const Orbit = () => {
 
 const Box = (props) => {
 	const ref = useRef(null);
-
 	useFrame(() => {
 		ref.current.rotation.x += 0.01;
 		ref.current.rotation.y += 0.01;
 	});
-
 	return (
-		//전달된 props를 rest parameter를 이용해 한꺼번에 모두적용 
-		<mesh ref={ref} {...props}>
+		<mesh ref={ref} {...props} >
 			<boxBufferGeometry />
-			<meshBasicMaterial color='blue' />
+			<meshPhysicalMaterial color='blue' />
+		</mesh>
+	)
+}
+
+const Floor = (props) => {
+	return (
+		<mesh {...props}>
+			<boxBufferGeometry args={[20, 1, 10]} />
+			<meshPhysicalMaterial color='white' />
 		</mesh>
 	)
 }
@@ -36,8 +41,10 @@ function App() {
 				camera={{ position: [7, 7, 7] }}>
 				<axesHelper args={[6]} />
 				<Orbit />
-				{/* Box 컴포넌트 호출시 위치값을 props로 전달 */}
-				<Box position={[0, 0, 0]} />
+
+				<ambientLight intensity={0.3} />
+				<Box position={[0, 4, 0]} />
+				<Floor position={[0, 0, 0]} />
 			</Canvas>
 		</figure>
 	);
